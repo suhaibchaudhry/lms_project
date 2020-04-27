a921d5bb6389c9969380ce1485d0d59a5a8baf7d
Suhaib Chaudhry <chaudhry.suhaib@gmail.com>
2020-04-27 02:48:19 -0500

    added courses list and ui to add/drop courses

--- student_dashboard.cpp
+++ student_dashboard.cpp
@@ -6,9 +6,40 @@ student_dashboard::student_dashboard(QWidget *parent) :
     ui(new Ui::student_dashboard)
 {
     ui->setupUi(this);
+
+    model = new QStringListModel(this);
+
+    QStringList list;
+    list << "CS1436" << "CS1437" << "CS2425" << "CS2436" << "CS1408" <<
+            "CS2301" << "CS2302" << "CS3306" << "CS3310" << "CS3321" <<
+            "CS4300" << "CS4315" << "CS4318";
+
+    model->setStringList(list);
+
+    ui->listView->setModel(model);
+
+    ui->listView->setEditTriggers(QAbstractItemView::NoEditTriggers);
 }
 
 student_dashboard::~student_dashboard()
 {
     delete ui;
+}
+
+void student_dashboard::on_pushButton_3_clicked()
+{
+    //add
+    int row = model->rowCount();
+    model->insertRows(row, 1);
+
+    QModelIndex index = model->index(row);
+
+    ui->listView->setCurrentIndex(index);
+    ui->listView->edit(index);
+}
+
+void student_dashboard::on_pushButton_2_clicked()
+{
+    //delete
+    model->removeRows(ui->listView->currentIndex().row(), 1);
 }
--- student_dashboard.h
+++ student_dashboard.h
@@ -1,6 +1,8 @@
 #ifndef STUDENT_DASHBOARD_H
 #define STUDENT_DASHBOARD_H
 
+#include <QtCore>
+#include <QtGui>
 #include <QDialog>
 
 namespace Ui {
@@ -15,8 +17,14 @@ public:
     explicit student_dashboard(QWidget *parent = nullptr);
     ~student_dashboard();
 
+private slots:
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::student_dashboard *ui;
+    QStringListModel *model;
 };
 
 #endif // STUDENT_DASHBOARD_H
