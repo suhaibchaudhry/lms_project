8e7f5a4cb25b46eba086eea3861847a028b15e1c
Suhaib Chaudhry <chaudhry.suhaib@gmail.com>
2020-05-02 21:35:16 -0500

    added window to enable admin to edit student information

--- admineditinfo.cpp
+++ admineditinfo.cpp
@@ -1,14 +1,197 @@
 #include "admineditinfo.h"
 #include "ui_admineditinfo.h"
+#include "variables.h"
+#include "mainwindow.h"
+#include <QMessageBox>
+#include <QFile>
+#include <QDebug>
+#include <QTextStream>
+#include <QRegularExpression>
+#include <QRegularExpressionMatch>
+#include <QRegularExpressionMatchIterator>
 
 admineditinfo::admineditinfo(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::admineditinfo)
 {
     ui->setupUi(this);
+
+    //set file for student info
+        QString filename = curr_path + user_id + "_info.dat";
+        QFile myFile(filename);
+
+        //open student info as file as read only
+        if(!myFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QMessageBox::critical(this, "Error", "User Not Found");
+        } else {
+            //start reading line by line and set the variable line to next line from file
+            QTextStream stream(&myFile);
+            QString line = stream.readLine();
+
+            //set the element placeholdertext to the line variable
+            ui->firstName->setPlaceholderText(line);
+
+            line = stream.readLine();
+            ui->lastName->setPlaceholderText(line);
+
+            line = stream.readLine();
+            ui->address->setPlaceholderText(line);
+
+            line = stream.readLine();
+            ui->city->setPlaceholderText(line);
+
+            line = stream.readLine();
+            ui->state->setPlaceholderText(line);
+
+            line = stream.readLine();
+            ui->zip->setPlaceholderText(line);
+
+            line = stream.readLine();
+            ui->dob->setPlaceholderText(line);
+
+            line = stream.readLine();
+            ui->phone->setPlaceholderText(line);
+        }
+
+        myFile.close();
 }
 
 admineditinfo::~admineditinfo()
 {
     delete ui;
+}
+
+void admineditinfo::on_pushButton_clicked()
+{
+    QString filename = curr_path + user_id + "_info.dat";
+    QFile myFile(filename);
+
+    //open file as readable and writable
+    myFile.open(QIODevice::ReadWrite | QIODevice::Text);
+
+    //create new qbytearray to store filedata and store it as a qstring called text
+    QByteArray fileData = myFile.readAll();
+    QString text(fileData);
+
+    //declare user inputted variables
+    QString user_fname;
+    QString user_lname;
+    QString user_address;
+    QString user_city;
+    QString user_state;
+    QString user_zip;
+    QString user_dob;
+    QString user_phone;
+
+    //set read pointer to beginning of file
+    myFile.seek(0);
+    QTextStream stream(&myFile);
+
+    QString line = stream.readLine();
+    user_fname = line;
+
+    line = stream.readLine();
+    user_lname = line;
+
+    line = stream.readLine();
+    user_address = line;
+
+    line = stream.readLine();
+    user_city = line;
+
+    line = stream.readLine();
+    user_state = line;
+
+    line = stream.readLine();
+    user_zip = line;
+
+    line = stream.readLine();
+    user_dob = line;
+
+    line = stream.readLine();
+    user_phone = line;
+
+    //checks if the label is empty
+    //if label is not empty, sets variable to the user inputted text
+    //and writes it to file
+    if(ui->firstName->text() != "") {
+        QString fname = ui->firstName->text();
+
+        //replaces user_fname with fname
+        text.replace(QRegularExpression(user_fname), QString(fname));
+
+        myFile.seek(0);
+
+        //writes to file as a qbytearray (.toUtf8() converts to qbytearray)
+        myFile.write(text.toUtf8());
+    }
+
+    if(ui->lastName->text() != "") {
+        QString lname = ui->lastName->text();
+        text.replace(QRegularExpression(user_lname), QString(lname));
+
+        myFile.seek(0);
+        myFile.write(text.toUtf8());
+    }
+
+    if(ui->address->text() != "") {
+        QString address = ui->address->text();
+        text.replace(QRegularExpression(user_address), QString(address));
+
+        myFile.seek(0);
+        myFile.write(text.toUtf8());
+    }
+
+    if(ui->city->text() != "") {
+        QString city = ui->city->text();
+        text.replace(QRegularExpression(user_city), QString(city));
+
+        myFile.seek(0);
+        myFile.write(text.toUtf8());
+    }
+
+    if(ui->state->text() != "") {
+        QString state = ui->state->text();
+        text.replace(QRegularExpression(user_state), QString(state));
+
+        myFile.seek(0);
+        myFile.write(text.toUtf8());
+    }
+
+    if(ui->zip->text() != "") {
+        QString zip = ui->zip->text();
+        text.replace(QRegularExpression(user_zip), QString(zip));
+
+        myFile.seek(0);
+        myFile.write(text.toUtf8());
+    }
+
+    if(ui->dob->text() != "") {
+        QString dob = ui->dob->text();
+        text.replace(QRegularExpression(user_dob), QString(dob));
+
+        myFile.seek(0);
+        myFile.write(text.toUtf8());
+    }
+
+    if(ui->phone->text() != "") {
+        QString phone = ui->phone->text();
+        text.replace(QRegularExpression(user_phone), QString(phone));
+
+        myFile.seek(0);
+        myFile.write(text.toUtf8());
+    }
+
+    myFile.close();
+
+    hide();
+    admin_dashboard *admin_dboard = new admin_dashboard(this);
+    admin_dboard->show();
+}
+
+void admineditinfo::on_pushButton_2_clicked()
+{
+    hide();
+    admin_dashboard *admin_dboard = new admin_dashboard(this);
+    admin_dboard->show();
 }
--- admineditinfo.h
+++ admineditinfo.h
@@ -15,6 +15,11 @@ public:
     explicit admineditinfo(QWidget *parent = nullptr);
     ~admineditinfo();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::admineditinfo *ui;
 };
